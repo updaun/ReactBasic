c3ed7ef02aece9fe7841f4577e97a08ffcfe8f79
updaun <updauney@daum.net>
2023-02-24 18:11:58 +0900

    #5.1 Tour of CRA
    use css module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-    </div>
+      <h1 className={styles.title}>Hello World</h1>
+      <Button text={"Continue"} />
+    </div >
   );
 }
 
