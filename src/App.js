25bb06e12a3376032f63f42b00fb72cf77cfe9fa
updaun <updauney@daum.net>
2023-03-03 13:10:36 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -2,30 +2,40 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byeFn() {
+    console.log("destroyed :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+  useEffect(() => {
+    console.log("created :)");
+    return () => {
+      console.log("destroyed :(");
+    };
+  }, []);
+  useEffect(function () {
+    console.log("created :)");
+    return function () {
+      console.log("destroyed :(");
+    };
+  }, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => { setKeyword(event.target.value) };
-  console.log("i run all the time")
-  const iRunOnlyOnce = () => {
-    console.log("i run only once");
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
   };
-  useEffect(iRunOnlyOnce, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword])
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter])
-  useEffect(() => {
-    console.log("I run when keyword & counter changes.");
-  }, [keyword, counter])
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div >
   );
 }
