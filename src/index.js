c219a099a6886316ee5372436a52145f1462c968
updaun <updauney@daum.net>
2023-03-07 23:26:32 +0900

    #7.3 Movie App part One

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CoinTracker from './CoinTracker';
+import MovieApp from './MovieApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <CoinTracker />
+    {/* <CoinTracker /> */}
+    <MovieApp />
   </React.StrictMode>
 );
