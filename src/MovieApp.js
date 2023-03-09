41cef0177681fa1b5ceba814f54c50ed71a6dcd8
updaun <updauney@daum.net>
2023-03-09 17:06:44 +0900

    #7.5 React Router

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -1,5 +1,18 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function MovieApp() {
-    return null;
+    return <Router>
+        <Routes>
+            <Route path="/movie" element={<Detail />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
 }
 
 export default MovieApp;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Movie({ CoverImg, title, summary, genres }) {
     return (
         <div>
             <img src={CoverImg} alt={title} />
-            <h2>{title}</h2>
+            <h2><Link to="/movie">{title}</Link></h2>
             <p>{summary}</p>
             <ul>
                 {genres.map((g) => <li key={g}>{g}</li>)}
