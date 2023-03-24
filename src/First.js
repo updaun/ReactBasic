bd600d9e15545ea6b5efaf6db196a73ec93e3eec
updaun <updauney@daum.net>
2023-03-24 13:25:35 +0900

    #1.2 useInput part Two

--- src/First.js
+++ src/First.js
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from "react";
 
-const useInput = (initalValue) => {
+const useInput = (initalValue, validator) => {
     const [value, setValue] = useState(initalValue);
     const onChange = (event) => {
-        console.log(event.target);
+        const {
+            target: { value }
+        } = event;
+        let willUpdate = true;
+        if (typeof validator === "function") {
+            willUpdate = validator(value);
+        }
+        if (willUpdate) {
+            setValue(value);
+        }
     }
     return { value, onChange };
 };
 
 const First = () => {
-    const name = useInput("Mr.");
+    // 최대 글자 수 제한
+    // const maxLen = value => value.length <= 10;
+    // const name = useInput("Mr.", maxLen);
+
+    // 특정 문자열 사용 제한
+    const isInclude = value => !value.includes("@");
+    const name = useInput("Mr.", isInclude);
+
     return (
         <div>
             <h1>First</h1>
