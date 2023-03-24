5af94166b2e445f4d5e22784909016acd9a669a6
updaun <updauney@daum.net>
2023-03-24 12:57:48 +0900

    #1.1 useInput

--- src/First.js
+++ src/First.js
@@ -1,50 +1,23 @@
 import React, { useState, useEffect } from "react";
 
+const useInput = (initalValue) => {
+    const [value, setValue] = useState(initalValue);
+    const onChange = (event) => {
+        console.log(event.target);
+    }
+    return { value, onChange };
+};
+
 const First = () => {
-    const [item, setItem] = useState(1);
-    const incrementItem = () => setItem(item + 1);
-    const decrementItem = () => setItem(item - 1);
+    const name = useInput("Mr.");
     return (
         <div>
-            <h1>First{item}</h1>
-            <h2>Start</h2>
-            <button onClick={incrementItem}>Increment</button>
-            <button onClick={decrementItem}>Decrement</button>
+            <h1>First</h1>
+            {/* <input placeholder="Name" value={name.value} onChange={name.onChange} /> */}
+            <input placeholder="Name" {...name} />
         </div>
     )
 }
 
-class AppUgly extends React.Component {
-    state = {
-        item: 1
-    }
-    render() {
-        const { item } = this.state;
-        return (
-            <div>
-                <h1>First{item}</h1>
-                <h2>Start</h2>
-                <button onClick={this.incrementItem}>Increment</button>
-                <button onClick={this.decrementItem}>Decrement</button>
-            </div>
-        );
-    }
-    incrementItem = () => {
-        this.setState(state => {
-            return {
-                item: state.item + 1
-            }
-        })
-    }
-    decrementItem = () => {
-        this.setState(state => {
-            return {
-                item: state.item - 1
-            }
-        })
-    }
 
-}
-
-// export default First;
-export default AppUgly;
+export default First;
