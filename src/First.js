93d1d519f2131e3da024628581b5fcfa15e49eeb
updaun <updauney@daum.net>
2023-03-27 00:16:25 +0900

    #2.1 useTitle

--- src/First.js
+++ src/First.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 
+const useTitle = (initialTitle) => {
+    const [title, setTitle] = useState(initialTitle);
+    const updateTitle = () => {
+        const htmlTitle = document.querySelector("title");
+        htmlTitle.innerText = title;
+    };
+    useEffect(updateTitle, [title]);
+    return setTitle;
+};
+
 const First = () => {
-    const sayHello = () => console.log("Hello");
-    const [number, setNumber] = useState(0);
-    const [aNumber, setAnumber] = useState(0);
-    useEffect(sayHello, [number]);
+    const titleUpdater = useTitle("Loading...");
+    setTimeout(() => titleUpdater("Home"), 5000);
     return (
         <div>
             <div>Hi</div>
-            <button onClick={() => setNumber(number + 1)}>{number}</button>
-            <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
         </div>
     )
 }
