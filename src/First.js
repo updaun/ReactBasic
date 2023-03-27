7cba21a957acc1abf5e917c67d2f33f24e7c4963
updaun <updauney@daum.net>
2023-03-28 05:04:41 +0900

    #2.5.1 useNetwork

--- src/First.js
+++ src/First.js
@@ -1,28 +1,32 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
-const useFadeIn = (duration = 1, delay = 0) => {
-    const element = useRef();
-    useEffect(() => {
-        if (typeof duration !== "number" || duration < 0 || typeof delay !== "number") {
-            return;
+const useNetwork = (onChange) => {
+    const [status, setStatus] = useState(navigator.onLine);
+    const handleChange = () => {
+        if (typeof onChange === "function") {
+            onChange(navigator.onLine);
         }
-        if (element.current) {
-            const { current } = element;
-            current.style.opacity = element;
-            current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`
-            current.style.opacity = 1;
+        setStatus(navigator.onLine);
+    }
+    useEffect(() => {
+        window.addEventListener("online", handleChange);
+        window.addEventListener("offline", handleChange);
+        return () => {
+            window.removeEventListener("online", handleChange);
+            window.removeEventListener("offline", handleChange);
         };
     }, []);
-    return { ref: element, style: { opacity: 0 } };
+    return status;
 }
 
 const First = () => {
-    const fadeInH1 = useFadeIn(1, 2);
-    const fadeInP = useFadeIn(5, 10);
+    const handleNetworkChange = (online) => {
+        console.log(online ? "We just went online" : "We are offline");
+    }
+    const onLine = useNetwork(handleNetworkChange);
     return (
         <div>
-            <h1 {...fadeInH1}>Hi</h1>
-            <p {...fadeInP}>lorem ipsum lalala</p>
+            <h1>{onLine ? "Online" : "Offline"} </h1>
         </div>
     )
 }
