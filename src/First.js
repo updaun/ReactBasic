3295111dea49ade5091a883236db49fda9c52568
updaun <updauney@daum.net>
2023-03-27 22:45:52 +0900

    #2.3 useConfirm

--- src/First.js
+++ src/First.js
@@ -1,35 +1,28 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
-const useHover = (onHover) => {
-    const element = useRef();
-    // componentDidMount
-    useEffect(() => {
-        if (typeof onHover !== "function") {
-            return;
+const useConfirm = (message = "", onConfirm, onCancel) => {
+    if (!onConfirm || typeof onConfirm !== "function") {
+        return;
+    }
+    if (onCancel && typeof onCancel !== "function") {
+        return;
+    }
+    const confirmAction = () => {
+        if (window.confirm(message)) {
+            onConfirm();
+        } else {
+            onCancel();
         }
-        // add event listener when component mount
-        if (element.current) {
-            element.current.addEventListener("mouseenter", onHover);
-        }
-        // clean up when component unmount
-        // componentWillUnmount
-        // component가 mount되지 않았을때 eventListener를 배치하지 않기 위해
-        return () => {
-            if (element.current) {
-                element.current.removeEventListener("mouseenter", onHover);
-            }
-        }
-    }, []);
-    return element;
-};
+    }
+    return confirmAction;
+}
 
 const First = () => {
-    const sayHello = () => console.log("say hello");
-    const title = useHover(sayHello);
+    const deleteWorld = () => console.log("Deleting the world...");
+    const abort = () => console.log("Aborted");
+    const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
     return (
-        <div>
-            <h1 ref={title}>Hi</h1>
-        </div>
+        <button onClick={confirmDelete}>Delete the world</button>
     )
 }
 
