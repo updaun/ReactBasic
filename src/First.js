0091360c3722a38d5646a81da504bae22d4ccd35
updaun <updauney@daum.net>
2023-03-29 14:44:20 +0900

    #2.7 useNotification

--- src/First.js
+++ src/First.js
@@ -1,62 +1,32 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const useFullscreen = (callback) => {
-    const element = useRef();
-    const runCb = isFull => {
-        if (callback && typeof callback === "function") {
-            callback(isFull);
+const useNotification = (title, options) => {
+    if (!("Notification" in window)) {
+        return;
+    }
+    const fireNotif = () => {
+        if (Notification.permission !== "granted") {
+            Notification.requestPermission().then(permission => {
+                if (permission === "granted") {
+                    new Notification(title, options);
+                } else {
+                    return;
+                }
+            })
+        } else {
+            new Notification(title, options);
+
         }
     }
-    const triggerFull = () => {
-        if (element.current) {
-            // for safari
-            if (element.current.requestFullscreen) {
-                element.current.requestFullscreen();
-                // for firefox
-            } else if (element.current.mozRequestFullScreen) {
-                element.current.mozRequestFullScreen();
-                // for chrome
-            } else if (element.current.webkitRequestFullscreen) {
-                element.current.webkitRequestFullscreen();
-                // for IE
-            } else if (element.current.msRequestFullscreen) {
-                element.current.msRequestFullscreen();
-            }
-            runCb(true);
-        }
-    }
-    const exitFull = () => {
-        document.exitFullscreen();
-        // for safari
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-            // for firefox
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-            // for chrome
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-            // for IE
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-        runCb(false);
-    }
-    return { element, triggerFull, exitFull };
+    return fireNotif;
 }
 
 const First = () => {
-    const onFullS = (isFull) => {
-        console.log(isFull ? "We are full" : "We are small");
-    }
-    const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+    const triggerNotif = useNotification("Can I steal your kimchi?", { body: "I love kimchi don't you?" });
+
     return (
         <div>
-            <div ref={element}>
-                <img src="https://media.giphy.com/media/3o7TKSjRrfIPjeUGO8/giphy.gif" alt="gif" />
-                <button onClick={exitFull}>Exit fullscreen</button>
-            </div>
-            <button onClick={triggerFull}>Make fullscreen</button>
+            <button onClick={triggerNotif}>Hello</button>
         </div>
     )
 }
