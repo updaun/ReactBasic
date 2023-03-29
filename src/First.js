a1c18868de4a115cf517605d36b8507ba37236dd
updaun <updauney@daum.net>
2023-03-29 14:11:46 +0900

    #2.6.1 useFullscreen

--- src/First.js
+++ src/First.js
@@ -1,26 +1,62 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const useScroll = () => {
-    const [state, setState] = useState({
-        x: 0,
-        y: 0
-    });
-    const onScroll = () => {
-        console.log("y", window.scrollY, "x", window.scrollX);
-        setState({ y: window.scrollY, x: window.scrollX });
+const useFullscreen = (callback) => {
+    const element = useRef();
+    const runCb = isFull => {
+        if (callback && typeof callback === "function") {
+            callback(isFull);
+        }
     }
-    useEffect(() => {
-        window.addEventListener("scroll", onScroll);
-        return () => window.removeEventListener("scroll", onScroll);
-    }, []);
-    return state;
-};
+    const triggerFull = () => {
+        if (element.current) {
+            // for safari
+            if (element.current.requestFullscreen) {
+                element.current.requestFullscreen();
+                // for firefox
+            } else if (element.current.mozRequestFullScreen) {
+                element.current.mozRequestFullScreen();
+                // for chrome
+            } else if (element.current.webkitRequestFullscreen) {
+                element.current.webkitRequestFullscreen();
+                // for IE
+            } else if (element.current.msRequestFullscreen) {
+                element.current.msRequestFullscreen();
+            }
+            runCb(true);
+        }
+    }
+    const exitFull = () => {
+        document.exitFullscreen();
+        // for safari
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+            // for firefox
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+            // for chrome
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+            // for IE
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+        runCb(false);
+    }
+    return { element, triggerFull, exitFull };
+}
 
 const First = () => {
-    const { y } = useScroll();
+    const onFullS = (isFull) => {
+        console.log(isFull ? "We are full" : "We are small");
+    }
+    const { element, triggerFull, exitFull } = useFullscreen(onFullS);
     return (
-        <div style={{ height: "1000vh" }}>
-            <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+        <div>
+            <div ref={element}>
+                <img src="https://media.giphy.com/media/3o7TKSjRrfIPjeUGO8/giphy.gif" alt="gif" />
+                <button onClick={exitFull}>Exit fullscreen</button>
+            </div>
+            <button onClick={triggerFull}>Make fullscreen</button>
         </div>
     )
 }
